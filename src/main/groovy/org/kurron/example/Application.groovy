4ed6542cee68abe6bd9b2e3d5d3c2c01a2855678
Ron Kurr <kurr@kurron.org>
2016-03-25 17:16:55 +0000

    Created the IntelliJ files.

--- src/main/groovy/org/kurron/example/Application.groovy
+++ src/main/groovy/org/kurron/example/Application.groovy
@@ -1,33 +1,31 @@
+/*
+ * Copyright (c) 2016. Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kurron.example
 
 import groovy.util.logging.Slf4j
-import org.aopalliance.aop.Advice
-import org.springframework.amqp.core.Binding
-import org.springframework.amqp.core.Declarable
-import org.springframework.amqp.core.DirectExchange
-import org.springframework.amqp.core.Queue
-import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
-import org.springframework.amqp.rabbit.connection.ConnectionFactory
-import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
-import org.springframework.context.annotation.Bean
-import org.springframework.retry.backoff.ExponentialRandomBackOffPolicy
-import org.springframework.retry.interceptor.StatefulRetryOperationsInterceptor
-
-import static org.springframework.amqp.core.Binding.DestinationType.QUEUE
 
 /**
  * The entry point into the system.  Runs as a standalone web server.
  */
 @Slf4j
 @SpringBootApplication
-@EnableCircuitBreaker
 @EnableConfigurationProperties( ApplicationProperties )
 class Application {
 
@@ -35,46 +33,4 @@ class Application {
         SpringApplication.run( Application, args )
     }
 
-    //TODO: move the dead-letter stuff to the configuration
-    @Bean
-    public List<Declarable> amqpBindings( ApplicationProperties configuration ) {
-        [
-          new DirectExchange( configuration.exchangeName ),
-          new Queue( configuration.queueName ),
-          new Binding( configuration.queueName, QUEUE, configuration.exchangeName, configuration.queueName, null ),
-          new DirectExchange( 'dead-letter' ),
-          new Queue( 'dead-letter' ),
-          new Binding( 'dead-letter', QUEUE, 'dead-letter', 'dead-letter', null )
-        ] as List<Declarable>
-    }
-
-    //TODO: put in a test to validate the dead letter stuff is working
-    @Bean
-    StatefulRetryOperationsInterceptor interceptor( RabbitTemplate template, ApplicationProperties settings ) {
-        def strategy = new RepublishMessageRecoverer( template, 'dead-letter', 'dead-letter' )
-        RetryInterceptorBuilder.stateful()
-                .maxAttempts( 3 )
-                .backOffPolicy( new ExponentialRandomBackOffPolicy() )
-                .recoverer( strategy )
-                .build()
-    }
-
-    @Bean
-    Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-        new Jackson2JsonMessageConverter()
-    }
-
-
-    @Bean
-    SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory( Jackson2JsonMessageConverter converter,
-                                                                         ConnectionFactory connectionFactory,
-                                                                         StatefulRetryOperationsInterceptor interceptor ) {
-        // for some reason, the Spring Boot auto configuration does not enable JSON serialization so we do it here
-        new SimpleRabbitListenerContainerFactory().with {
-            setMessageConverter( converter )
-            setConnectionFactory( connectionFactory )
-            setAdviceChain( [interceptor] as Advice[] )
-            it
-        }
-    }
 }
