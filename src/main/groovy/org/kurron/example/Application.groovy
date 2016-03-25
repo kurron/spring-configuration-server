ae67145dbd962460993ee02e85ace6f6cd93344c
Ron Kurr <kurr@kurron.org>
2016-03-25 17:28:20 +0000

    We can stand up the server.

--- src/main/groovy/org/kurron/example/Application.groovy
+++ src/main/groovy/org/kurron/example/Application.groovy
@@ -16,17 +16,15 @@
 
 package org.kurron.example
 
-import groovy.util.logging.Slf4j
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.cloud.config.server.EnableConfigServer
 
 /**
  * The entry point into the system.  Runs as a standalone web server.
  */
-@Slf4j
 @SpringBootApplication
-@EnableConfigurationProperties( ApplicationProperties )
+@EnableConfigServer
 class Application {
 
     static void main( String[] args ) {
